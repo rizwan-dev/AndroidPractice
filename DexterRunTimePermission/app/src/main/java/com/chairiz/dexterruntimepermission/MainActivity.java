08e005abdc6918e9d2de4eecea718d17f84e15e4
Chaitali Karewar <karewarchaitali@gmail.com>
2018-03-28 22:31:13 +0530

    Added run permission project

---
+++ DexterRunTimePermission/app/src/main/java/com/chairiz/dexterruntimepermission/MainActivity.java
@@ -0,0 +1,13 @@
+package com.chairiz.dexterruntimepermission;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
